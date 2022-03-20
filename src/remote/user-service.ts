e85a6682d3de143f317ee21ce73f17f1119d6d61
abhilekhx <lekh7adhikari@gmail.com>
2022-03-20 01:05:12 -0400

    listed out some user-service methods

---
+++ src/remote/user-service.ts
@@ -0,0 +1,13 @@
+import { appClient } from "./app-client";
+
+export const register = (userInfo:object) => {
+    // TODO implement axios call to POST /users
+}
+
+export const updateUser = () => {
+    // TODO implement axios call to PUT /users
+}
+
+export const activateUser = (userId: string) => {
+    
+}
