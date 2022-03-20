f802f3922947a1b6d213fb7d79e9f6356b6461a1
abhilekhx <lekh7adhikari@gmail.com>
2022-03-20 00:18:17 -0400

    1) Changed Login and Register to LoginForm and RegisterForm. 2) Changed LoginForm path to /login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 import Dashboard from './components/Dashboard';
 import { Principal } from './models/principal';
 
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login currentUser={authUser} setCurrentUser={setAuthUser}/>}/>
-          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<LoginForm currentUser={authUser} setCurrentUser={setAuthUser}/>}/>
+          <Route path="/register" element={<RegisterForm/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
       </BrowserRouter>
