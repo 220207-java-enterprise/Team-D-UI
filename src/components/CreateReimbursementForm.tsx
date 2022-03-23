308d09e71b5a7baac29b990a923b65246248cd2c
Ryan Odeneal <ryan063@revature.net>
2022-03-23 17:45:57 -0500

    Not working

---
+++ src/components/CreateReimbursementForm.tsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import React, {SyntheticEvent, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { appClient } from "../remote/app-client";
+import '../styles/styles.css'
+import ErrorMessage from "./ErrorMessage";
+
+interface ICreateReimbursementFormProps {
+    createMode : boolean | undefined,
+    setCreateMode: (createForm: any | undefined) => void
+}
+
+function CreateReimbursementForm(props: ICreateReimbursementFormProps) {
+
+
+
+    const [formInfo, setFormInfo] = useState({
+        amount: 0,
+        description: "",
+        reimbursementType: "OTHER",
+        receipt: null
+    });
+
+  
+
+    const [errorMsg, setErrorMsg] = useState("");
+
+
+
+    return (
+        <div className="background">
+        <h1 className="page-heading pt-5">Register Form</h1>
+        <form
+            className="container d-flex justify-content-around"
+            // onSubmit={submitHandler}
+        >
+
+            <div className="justify-content-center">
+                <div className="form-group m-3">
+                    <input 
+                        name="amount"
+                        type="number" 
+                        required
+                        value={formInfo.amount}
+                        placeholder="enter amount..." 
+                        className="form-control" 
+                        // onChange={changeHandler}
+                    />
+                </div>
+
+                <div className="form-group m-3">
+                    <input
+                        name="description" 
+                        type="textbox" 
+                        required 
+                        value={formInfo.description}
+                        placeholder="enter description..." 
+                        className="form-control" 
+                        // onChange={changeHandler}
+                    />
+                </div>
+                
+
+                
+                <div className="form-group m-3">
+                    <h3 className="white-text">Select Type:</h3>
+                    <select
+                        name="reimbursementType" 
+                        // onChange={changeHandler} 
+                        id="type-select" 
+                        className="form-select form-select-lg">
+                        <option value="LODGING"> 
+                            Lodging 
+                        </option>
+                        <option value="TRAVEL">
+                            Travel
+                        </option>
+                        <option value="FOOD">
+                            Food
+                        </option>
+                        <option value="OTHER">
+                            Other
+                        </option>
+                    </select>
+                </div>
+
+                <input 
+                    className="btn-lg btn-primary" 
+                    type="submit"
+                    value="Create"
+                    onClick= { props.setCreateMode(!props.createMode);}
+                />
+            </div>                
+        </form>
+
+        {errorMsg? <ErrorMessage errorMessage={errorMsg}/> : <></>}
+      </div>
+
+    );
+
+}
+
+export default CreateReimbursementForm;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,12 +2,16 @@ import React, {useState, useEffect} from 'react';
 import { Principal } from '../models/principal';
 import '../styles/styles.css'
 import DataGridTable from './DataGridTable';
+import CreateReimbursementForm from './CreateReimbursementForm';
 
 interface IDashboardProps{
     currentUser : Principal | undefined,
 }
 
+
 function Dashboard(props: IDashboardProps){
+
+    const[createMode, setCreateMode] = useState<boolean>(false);
 
     return(
         !props.currentUser ? <>Login Required!</> :
@@ -16,10 +20,20 @@ function Dashboard(props: IDashboardProps){
             <h1 className="page-heading pt-5">Dashboard</h1>
             <div className="container white-text">
                 <h3>Welcome, {props.currentUser.username}</h3>
+               
                 <h4>{props.currentUser.role} #{props.currentUser.id}</h4>
                 <p>{props.currentUser.token}</p>
             </div>
             <DataGridTable authUser={props.currentUser}/>
+            {props.currentUser.role == "EMPLOYEE" ? <input 
+                                                    className="btn-lg btn-primary" 
+                                                    type="submit"
+                                                    value="Create"
+                                                    onSubmit=
+                                            /> : ""}
+            {createMode? 
+            <CreateReimbursementForm 
+            createMode={createMode} setCreateMode={setCreateMode}/>: ""}
         </div>
     )
 }
